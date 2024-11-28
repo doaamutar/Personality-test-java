87682fc07e1a5424b5e6659365c8d39f9db0429e
doaamutar <doaamutar7@gmail.com>
2024-11-28 13:37:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -46,49 +46,6 @@
  }
  
  /**
-  * Represents a completed test result
-  */
- class TestResult implements Comparable<TestResult> {
-     private Date testDate;
-     private Map<PersonalityDimension, Integer> scores;
-     private String userId;
-     
-     public TestResult(String userId) {
-         this.userId = userId;
-         this.testDate = new Date();
-         this.scores = new EnumMap<>(PersonalityDimension.class);
-     }
-     
-     public void setScore(PersonalityDimension dimension, int score) {
-         scores.put(dimension, score);
-     }
-     
-     public int getScore(PersonalityDimension dimension) {
-         return scores.getOrDefault(dimension, 0);
-     }
-     
-     public Date getTestDate() { return testDate; }
-     public String getUserId() { return userId; }
-     
-     @Override
-     public int compareTo(TestResult other) {
-         return this.testDate.compareTo(other.testDate);
-     }
-     
-     @Override
-     public String toString() {
-         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-         StringBuilder sb = new StringBuilder();
-         sb.append("Test Date: ").append(sdf.format(testDate)).append("\n");
-         sb.append("User ID: ").append(userId).append("\n");
-         for (PersonalityDimension dim : PersonalityDimension.values()) {
-             sb.append(dim.getName()).append(": ").append(scores.get(dim)).append("/40\n");
-         }
-         return sb.toString();
-     }
- }
- 
- /**
   * Main system class that manages the personality test
   */
  class PersonalityTestSystem {
