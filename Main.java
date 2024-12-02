6090a6d5650935b74134109d8479cb8bb4fc68c4
doaamutar <doaamutar7@gmail.com>
2024-12-02 15:24:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+    public class Main {
+        public static Scanner scanner = new Scanner(System.in);
+        private static PersonalityTestSystem system = new PersonaliyTestSystem();
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Personality Test System");
+
+            System.out.print("Are you an Admin or a User?");
+            String answer = scanner.nextLine();
+
+            if (answer.equals("Admin")){
+                boolean isAdmin = PersonalityTestSystem.checkPassword(scanner);
+                    while (true){
+                        System.out.println("")
+                    }
+
+            }
+
+            boolean isAdmin = PersonalityTestSystem.checkPassword(scanner);
+        }
     
-     
+    }
